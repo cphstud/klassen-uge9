e2fda6afc990c1ac4456d296c5e7ddf9f33e2642
thor <thor@cphbusiness.dk>
2020-02-27 11:32:08 +0100

    opg

--- Opg73.java
+++ Opg73.java
@@ -1,12 +1,18 @@
 public class Opg73 {
   public static int indexOfMax(int[] inputArr) {
     int retVal = 0;
-
-  
+    for(int i=0;i<inputArr.length;i++) {
+      if (inputArr[i]>retVal) {
+        retVal=i;
+      }
+    }
     return retVal;
   }
   public static void  main(String[] args) {
-    int maxVal = indexOfMax(new int[] {1,2,3,4,5}); 
+    int[] testArr = new int[]{1,3,5,4,3,1,77,3,2};
+    int maxVal = indexOfMax(new int[] {1,2,9,2,3,4,5}); 
+     maxVal = indexOfMax(testArr);
+    System.out.println("max: " + maxVal + " value: "+ testArr[maxVal] );
   }
 
 }
