94eaca5809b2678c8f122ee4cd3a119940f0567b
thor <thor@cphbusiness.dk>
2020-02-28 00:55:55 +0100

    klassen uge9

---
+++ Test4SeqOpg.java
@@ -0,0 +1,14 @@
+public class Test4SeqOpg {
+  /*
+1 2 3 5 8 13 21 34
+1 2 4 8 16 32 64 128
+1 3 7 15 31 63 127 255
+   */
+  public static final int MAX = 8;
+  public static void main(String[] args) {
+
+    int[] myArr = new int[MAX];
+    int[] myArr2 = new int[MAX];
+    int[] myArr3 = new int[MAX];
+  }
+}
